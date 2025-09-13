fcab4b07d13266bfd95753f56746dfb624c13259
muratcan-yuksel <muratcanyukselpro@gmail.com>
2025-09-14 00:04:02 +0300

    starters

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TotalCustomers from "./Dashboard/TotalCustomers";
+
+const App = () => {
+  return (
+    <div>
+      <TotalCustomers />
+    </div>
+  );
+};
+
+export default App;
